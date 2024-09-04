db2a17f5b930ff44f0ffe4bb09ae1b75cac8ef87
sairam <sairamvgraju@gmail.com>
2024-09-04 09:39:11 -0400

    integrated with calculon and timeloop

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-for proc_count in 512 1024 ; do
+for proc_count in 64 128 512 1024 2048; do
     PYTHONPATH=. ./bin/calculon llm-optimal-execution models/megatron-1T.json $proc_count 256 float16 systems/testsystem.json results/diff_tech_analysis/output_imec_${proc_count}_256.csv -m
 done
